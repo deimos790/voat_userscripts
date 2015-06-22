5c5725d704e14a30817056d97c72ee57bf78864e
MehranGhamaty <mnghamaty@gmail.com>
2015-06-22 13:57:09 -0700

    Updated navigation script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # voat_userscripts
 User scripts that make voat.co more usable to me.
+
+VoatImageOpener is a clone of RES "view images" button. It expands all links and discussion posts. Seems to be error free.
+
+VoatKeyboardNavigation has basic navigation down (move up down and expand). Currently there is an issue after reaching the end of the page.
+
+Scripts are for use with [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)  for firefox or [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) for chrome. 
+
+
--- VoatKeyboardNavigation.user.js
+++ VoatKeyboardNavigation.user.js
@@ -1,35 +1,45 @@
 // ==UserScript==
 // @name         VoatKeyboardNavigation
 // @namespace    http://mehranghamaty.com/
-// @version      0.1
-// @description  keyboard navigation still very buggy
+// @version      0.3
+// @description  keyboard navigation still very buggy, locks div to top. j goes down, k goes up, x expands the link
 // @author       Mehran Ghamaty
 // @match        https://voat.co/*
 // @grant        none
 // ==/UserScript==
 
-var currently_on = 0;
+//"constants"
 var highlighted_style = "thick solid #0000FF"
+var move_down = 74 //j
+var move_up = 75 //k
+var up_voat = 65 //a
+var down_voat = 90 //z
+var expand = 88 //x
+//TODO
+var comments = 67 //c
+var save = 83 //s
+
+//Global vars
+var currently_on = 0;
 var submissions = document.getElementsByClassName('submission');
+
+
 
 submissions[currently_on].style.border = highlighted_style;
 
 function moveDown(){
-    
   submissions[currently_on].style.border = "";
   
   if(currently_on < submissions.length)
     currently_on++;
     
-    
   submissions[currently_on].style.border = highlighted_style;
   submissions[currently_on].scrollIntoView();
     
-    
+  
 }
 
 function moveUp(){
-        
   submissions[currently_on].style.border = "";
     
   if(currently_on > 0)
@@ -39,12 +49,41 @@ function moveUp(){
   submissions[currently_on].scrollIntoView();
 }
 
+function openCurrentSubmission(){
+   var evt = document.createEvent ("HTMLEvents");
+   evt.initEvent ("click", true, true);
+   submissions[currently_on].getElementsByClassName('expando-button')[0].dispatchEvent(evt);
+}
+
+function upVoatCurrentSubmission(){
+   var evt = document.createEvent ("HTMLEvents");
+   evt.initEvent ("click", true, true);
+   submissions[currently_on].getElementsByClassName('arrow-upvote login-require')[0].dispatchEvent(evt);
+}
+
+function upVoatCurrentSubmission(){
+   var evt = document.createEvent ("HTMLEvents");
+   evt.initEvent ("click", true, true);
+   submissions[currently_on].getElementsByClassName('arrow-downvote login-required')[0].dispatchEvent(evt);
+}
+
 document.addEventListener('keydown', function(event) {
-    if(event.keyCode == 74) {
-        //alert('J was pressed');
+
+    if(event.keyCode == move_down) {
         moveDown();
-    } else if(event.keyCode == 75) {
-        //alert('K was pressed');
+    } else if(event.keyCode == move_up) {
         moveUp();
-    }
+    } else if(event.keyCode == expand) {
+        openCurrentSubmission();
+    }// else if(event.keyCode == up_voat) {
+    //    upVoatCurrentSubmission();
+    //} else if(event.keyCode == down_voat) {
+    //    downVoatCurrentSubmission();
+    //} else if(event.keyCode == comments) {
+    //   openCurrentComments();
+    //} else if(event.keyCode == save) {
+    //    saveCurrentSubmission();
+    //}
+    
+    
 });
